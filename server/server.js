f7ba02695c934b91cce2b72ddb025a5b7f30665e
imetallix <42738259+imetallix@users.noreply.github.com>
2018-09-09 14:57:22 +0300

    Set up for Heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -42,7 +43,7 @@ app.get('/todos/:id', (req,res) =>{
     })
 });
 
-var port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(`Started up on port ${port}`);
 });
