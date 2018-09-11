d850e3d78ccf45e5145face1ad8ea1310aed34ad
imetallix <42738259+imetallix@users.noreply.github.com>
2018-09-11 21:21:56 +0300

    Deploying to Google Cloud test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -14,8 +14,9 @@ if (nconf.get('mongoDatabase')) {
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(uri || 'mongodb://' +
-    'localhost:27017/TodosApp');
+mongoose.connect( 'mongodb://knat_ow:dor951753@ds245532.mlab.com:45532/rest-api-test'
+    || uri || 'mongodb://' +
+    'localhost:27017/TodosApp'  );
 
 module.exports.mongoose = {
     mongoose
